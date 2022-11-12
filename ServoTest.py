b714a095d862365f001a1d1ad77855229dfe70f8
josé Roberto Canuto <jr.robertocv@gmail.com>
2022-11-12 01:06:32 -0300

    adding Servo test

--- ServoTest.py
+++ ServoTest.py
@@ -16,10 +16,12 @@ Servo.start(2)
 
 for i in range(3,10,1):
 	Servo.ChangeDutyCycle(i)
+	time.sleep(0.25)
 	Servo.ChangeDutyCycle(50)
 	time.sleep(2)
 
 for i in range(10,3,-1):
 	Servo.ChangeDutyCycle(i)
+	time.sleep(0.25)
 	Servo.ChangeDutyCycle(50)
 	time.sleep(2)
