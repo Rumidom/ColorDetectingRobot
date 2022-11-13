7e30abb5409c5d407342616fbe0da151c8a5ca4d
josé Roberto Canuto <jr.robertocv@gmail.com>
2022-11-13 14:19:39 -0300

    adding rotation

--- ColorDetection.py
+++ ColorDetection.py
@@ -6,6 +6,46 @@ import time
 
 # sudo apt-get update && sudo apt-get install python3-pigpio
 # https://ben.akrin.com/raspberry-pi-servo-jitter/
+gpio.setmode(gpio.BCM)
+
+#Left Motor
+gpio.setup(20,gpio.OUT)
+gpio.setup(21,gpio.OUT)
+LeftFoward = gpio.PWM(20,100)
+LeftBackwards = gpio.PWM(21,100)
+LeftBackwards.start(0)
+LeftFoward.start(0)
+
+def LeftMotorMove(value):
+	if value > 0:
+		LeftBackwards.ChangeDutyCycle(0) 
+		LeftFoward.ChangeDutyCycle(value)
+	elif value < 0:
+		LeftFoward.ChangeDutyCycle(0) 
+		LeftBackwards.ChangeDutyCycle(abs(value))
+	elif value == 0:
+		LeftBackwards.ChangeDutyCycle(0) 
+		LeftFoward.ChangeDutyCycle(0) 
+
+#Right Motor
+gpio.setup(19,gpio.OUT)
+gpio.setup(16,gpio.OUT)
+RightFoward = gpio.PWM(16,100)
+RightBackwards = gpio.PWM(19,100)
+RightBackwards.start(0)
+RightFoward.start(0)
+
+def RightMotorMove(value):
+	if value > 0:
+		RightBackwards.ChangeDutyCycle(0) 
+		RightFoward.ChangeDutyCycle(value)
+	elif value < 0:
+		RightFoward.ChangeDutyCycle(0) 
+		RightBackwards.ChangeDutyCycle(abs(value))
+	elif value == 0:
+		RightBackwards.ChangeDutyCycle(0) 
+		RightFoward.ChangeDutyCycle(0) 
+
 
 #Configuring dont show warnings 
 gpio.setwarnings(False)
@@ -14,7 +54,7 @@ Servo = pigpio.pi()
 Servo.set_mode(servopin, pigpio.OUTPUT)
 Servo.set_PWM_frequency( servopin, 50 )
 ServoPostion = 800
-
+RobotRotation = 0
 cap = cv2.VideoCapture(0)
 Upper_Limit = np. array([117,255,201])
 Lower_Limit = np. array([101,71,47])
@@ -25,6 +65,8 @@ height = frame.shape[0]
 print("Starting")
 
 while True:
+	LeftMotorMove(0)
+	RightMotorMove(0)
 	ret, frame = cap.read()
 	hsv_img = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
@@ -40,11 +82,22 @@ while True:
 		cent_x = np.average(mass_x)
 		cent_y = np.average(mass_y)
 		yerror = cent_y - (height/2)
+		xerror = cent_x - (width/2)
 		NewServoPostion = ServoPostion+int(yerror*0.1)
 		if (NewServoPostion> 800) and (NewServoPostion < 2000):
 			ServoPostion = NewServoPostion
 			Servo.set_servo_pulsewidth( servopin,  ServoPostion)
-		print(yerror)
+		
+		RobotRotation = int(xerror*0.5)
+		if RobotRotation > 100:
+			RobotRotation = 100
+		if RobotRotation < -100:
+			RobotRotation = -100
+
+			LeftMotorMove(RobotRotation)
+			RightMotorMove(-RobotRotation)
+
+		print(yerror,xerror,len(mass_y))
 		#frame = cv2.putText(frame, str(int(yerror*0.1)), (50, 50), cv2.FONT_HERSHEY_SIMPLEX,1, (255, 0, 0), 2, cv2.LINE_AA)
 		#cv2.circle(frame, [int(cent_x), int(cent_y)], 7, (0, 255, 0), -1)
 	
